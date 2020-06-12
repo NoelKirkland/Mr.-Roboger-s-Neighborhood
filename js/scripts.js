25922339c2cbcae895e3adeab973d5dbaedc1954
Noel Kirkland <noelkirkland@gmail.com>
2020-06-12 09:33:12 -0700

    test a while loop to see if it can return a countdown array from our users input. Honestly, I am really not sure how to do this one. I have a hard time wrapping my head around loops this week and I am honstly not sure where to start.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,14 @@
 
 // User interface
 $(document).ready(function(){
-  event.preventDefault();
-
+  $("form#the-neighborhood").submit(function(event){
+    const usersNumber = parseInt($("input#input-number").val());
+while (usersNumber > 0) {
+  console.log(usersNumber);
+  usersNumber --;
+}
+console.log("When I get to zero I'm going to come over there, neighbor!")
+    
+    event.preventDefault();
+  });
 });
