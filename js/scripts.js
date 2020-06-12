16a7b2d0e451f6c91e760b9d6f46e128650b3eb7
Noel Kirkland <noelkirkland@gmail.com>
2020-06-12 11:47:37 -0700

    in a really wet way, complete what the promt is asking for. I am going to continue to refactor it. Spec 6 complete'

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,12 @@ $(document).ready(function(){
     }
     let sequentialArray = numberArray.reverse();
     for (var index = 0; index < sequentialArray.length; index++)
-    if (sequentialArray[index] === 1)
+    if (sequentialArray[index] === 1 || sequentialArray[index] === 11 || sequentialArray[index] === 14 || sequentialArray[index] === 15  || sequentialArray[index] === 16 || sequentialArray[index] === 17 || sequentialArray[index] === 18 || sequentialArray[index] === 19)
         sequentialArray[index] = "Beep!"
-    else if (sequentialArray[index] === 2)
-    sequentialArray[index] = "Boop!";
+    else if (sequentialArray[index] === 2 || sequentialArray[index] === 12 || sequentialArray[index] === 20 || sequentialArray[index] === 21 || sequentialArray[index] === 22 || sequentialArray[index] === 24)
+    sequentialArray[index] = "Boop!"
+    else if (sequentialArray[index] === 3 || sequentialArray[index] === 13 || sequentialArray[index] === 23)
+    sequentialArray[index] = "Won't you be my neighbor?";
     // let beep = sequentialArray.map(function(element){
     //   return replace(element === 1, "Beep!")
     // });
