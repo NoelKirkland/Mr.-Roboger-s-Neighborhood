cc1ab13ca5cb79b589d42ae1ab26a0b576ba4a81
Noel Kirkland <noelkirkland@gmail.com>
2020-06-20 14:10:06 -0700

    make fairly substantial changes to the js logic in order to make my application scalable. I have though implemented a max number parameter in my html that is set at 1,000. Application ready for resubmission.

--- js/scripts.js
+++ js/scripts.js
@@ -9,34 +9,30 @@ $(document).ready(function(){
     $("#output-reversed").fadeToggle();
   });
   $("form#the-neighborhood").submit(function(event){
-    // Collect initial user input
-    const usersNumber = parseInt($("input#input-number").val());
-    if(!usersNumber || usersNumber <= 0 || usersNumber > 24){
-      alert("Please enter a number between 1 and 24");
-    }
-    // Create an array of numbers starting from input number to 0
+    event.preventDefault();
+
+    const userNumber = $("input#input-number").val();
+
     let numberArray =[];
-    for (let index = 0; index <= usersNumber; index ++) {
-      numberArray.push(usersNumber - [index]);
-    }
+    console.log(numberArray);
 
-    // Reverse order of array so that it starts at 0 and goes to input number
-    let sequentialArray = numberArray.reverse();
+    for (var index = 1; index <= userNumber; index++){
+    let userNumber = index + "";
+      if (userNumber.includes(3)){
+        numberArray.push("Won't you be my neighbor?");
+      }else if (userNumber.includes(2)){
+        numberArray.push("Boop!");
+      }else if (userNumber.includes(1)){
+        numberArray.push("Beep!");
+      }else {
+        numberArray.push(index); 
+      };
+    };
 
-    // Loop through array and redefine elements outlined in the branches below
-    for (var index = 0; index < sequentialArray.length; index++)
-    if (sequentialArray[index] === 1 || sequentialArray[index] === 10 || sequentialArray[index] === 11 || sequentialArray[index] === 14 || sequentialArray[index] === 15  || sequentialArray[index] === 16 || sequentialArray[index] === 17 || sequentialArray[index] === 18 || sequentialArray[index] === 19)
-        sequentialArray[index] = "Beep!"
-    else if (sequentialArray[index] === 2 || sequentialArray[index] === 12 || sequentialArray[index] === 20 || sequentialArray[index] === 21 || sequentialArray[index] === 22 || sequentialArray[index] === 24)
-    sequentialArray[index] = "Boop!"
-    else if (sequentialArray[index] === 3 || sequentialArray[index] === 13 || sequentialArray[index] === 23)
-    sequentialArray[index] = "Won't you be my neighbor?";
-
-    let results = toString(sequentialArray);
-    let resultsReversed = sequentialArray.reverse();
+    let results = toString(numberArray);
+    let resultsReversed = numberArray.reverse();
     $("#answer").fadeIn();
     $("#output").text(results);
     $("#output-reversed").text(resultsReversed); 
-    event.preventDefault();
-  });
+    });
 });
