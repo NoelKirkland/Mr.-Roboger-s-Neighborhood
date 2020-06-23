1f6ded2528d609069afaa5c0d39d568e4259f5fb
Noel Kirkland <noelkirkland@gmail.com>
2020-06-22 21:13:33 -0700

    make final edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
 // User interface
+function rogersTranslator (userNumber){
+  let numberArray =[];
+
+  for (var index = 1; index <= userNumber; index++){
+    let userNumber = index + "";
+    if (userNumber.includes(3)){
+      numberArray.push("Won't you be my neighbor?");
+    } else if (userNumber.includes(2)){
+      numberArray.push("Boop!");
+    } else if (userNumber.includes(1)){
+      numberArray.push("Beep!");
+    } else {
+      numberArray.push(index); 
+    };
+  };
+  return numberArray
+}
+
+// UI
 $(document).ready(function(){
   $("#reversed").click(function(){
     $("#output-reversed").fadeToggle();
@@ -6,24 +25,10 @@ $(document).ready(function(){
   $("form#the-neighborhood").submit(function(event){
     event.preventDefault();
 
-    const userNumber = $("input#input-number").val();
-    let numberArray =[];
-
-    for (var index = 1; index <= userNumber; index++){
-    let userNumber = index + "";
-      if (userNumber.includes(3)){
-        numberArray.push("Won't you be my neighbor?");
-      } else if (userNumber.includes(2)){
-        numberArray.push("Boop!");
-      } else if (userNumber.includes(1)){
-        numberArray.push("Beep!");
-      } else {
-        numberArray.push(index); 
-      };
-    };
-
-    let results = numberArray.toString();
-    let resultsReversed = numberArray.reverse();
+    const userNum = $("input#input-number").val();
+    let numArray = rogersTranslator(userNum)
+    let results = numArray.toString();
+    let resultsReversed = numArray.reverse();
     $("#answer").fadeIn();
     $("#output").text(results);
     $("#output-reversed").text(resultsReversed); 
